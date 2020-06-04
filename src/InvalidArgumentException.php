ca3c619050c2fa3cccd0984f1c00458d27ead88a
mf_tyrael <skugarev.andrey@gmail.com>
2020-06-04 11:31:50 +0300

    Add declare

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Yiisoft\Cache\Memcached;
 
 class InvalidArgumentException extends \RuntimeException implements \Psr\SimpleCache\InvalidArgumentException
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Yiisoft\Cache\Memcached\Tests;
 
 use Psr\SimpleCache\CacheInterface;
