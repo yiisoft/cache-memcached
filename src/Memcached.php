3b05c17acbcbe82add0b77dc8e51d7aab2447e5a
Alexander Makarov <sam@rmcreative.ru>
2020-09-11 01:32:53 +0300

    Comply to PSR-16
    The following characters are reserved for future extensions and MUST NOT be supported by implementing libraries: {}()/\@:

--- src/Memcached.php
+++ src/Memcached.php
@@ -272,7 +272,7 @@ final class Memcached implements CacheInterface
      */
     private function validateKey($key): void
     {
-        if (!\is_string($key)) {
+        if (!\is_string($key) || strpbrk($key, '{}()/\@:')) {
             throw new InvalidArgumentException('Invalid key value.');
         }
     }
